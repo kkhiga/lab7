8e61066ea145570922342f2738b18265cca6ed43
Kellie Higa <kkhiga@ucsd.edu>
2017-02-23 11:40:37 -0800

    Set up Google Analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(sendData);
+
+	function sendData(event) {
+		ga("send", "event", 'like', 'click');
+	}
 }
